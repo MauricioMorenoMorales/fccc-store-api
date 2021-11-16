3457bce52f70c33f8de143750d3d95a16723351d
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-11-16 09:58:44 -0600

    App index added

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log('04 Store API')
+require('dotenv').config();
+// async errors
+const express = require('express');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require('./middleware/not-found');
+const app = express();
+
+const connectDb = require('./db/connect.js');
+
+const port = process.env.PORT || 3333;
+
+//middleware
+app.use(express.json());
+
+// routes
+app.get('/', (req, res) => {
+	res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
+});
+
+app.use('/api/v1/products', )
+
+// products routes
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+(async () => {
+	try {
+		await connectDb();
+		app.listen(port, () => console.log(`App running on the port ${port}...`));
+	} catch (error) {
+		console.log(`Error starting the server: ${error}`);
+	}
+})();
--- db/connect.js
+++ db/connect.js
@@ -1,12 +1,22 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const connectDB = (url) => {
-  return mongoose.connect(url, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
-}
+const connectionEndpoint = (username, password, databaseName) =>
+	`mongodb+srv://${username}:${password}@nodejsplatzi.cg57m.mongodb.net/${databaseName}?retryWrites=true&w=majority`;
 
-module.exports = connectDB
+const connectDB = url => {
+	return mongoose.connect(
+		connectionEndpoint(
+			process.env.MONGODB_USERNAME,
+			process.env.MONGODB_PASSWORD,
+			process.env.MONGODB_DATABASE_NAME,
+		),
+		{
+			useNewUrlParser: true,
+			useCreateIndex: true,
+			useFindAndModify: false,
+			useUnifiedTopology: true,
+		},
+	);
+};
+
+module.exports = connectDB;
