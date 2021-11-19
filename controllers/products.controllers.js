0ac6f0c55b4708e6be0d1ca40124bea3d02b5647
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-11-19 08:37:20 -0600

    Some queries added and pagination added

--- controllers/products.controllers.js
+++ controllers/products.controllers.js
@@ -1,14 +1,44 @@
 const Product = require('../models/product.model');
 
-module.exports.getAllProducts = async (req, res) => {
-	const products = await Product.find(req.query);
-	res.status(200).json({ products, nbHits: products.length });
-};
-
 module.exports.getAllProductsStatic = async (req, res) => {
-	const products = await Product.find({});
+	const search = 'u';
+	const products = await Product.find({}).select('name price').limit(10);
 	res.status(200).json({
 		products,
 		nbHits: products.length,
 	});
+};
+
+module.exports.getAllProducts = async (req, res) => {
+	const { featured, company, name, sort, fields } = req.query;
+	const queryObject = {};
+	//Verifies te query params
+	if (featured) {
+		queryObject.featured = featured === 'true';
+	}
+	if (company) {
+		queryObject.company = company;
+	}
+	if (name) {
+		queryObject.name = { $regex: name, $options: 'i' };
+	}
+	let result = Product.find(queryObject);
+	if (sort) {
+		const sortList = sort.split(',').join(' ');
+		result = result.sort(sortList);
+	} else {
+		result = result.sort('createdAt');
+	}
+	if (fields) {
+		const fieldsList = fields.split(',').join(' ');
+		result = result.select(fieldsList);
+	}
+
+	const page = Number(req.query.page) || 1;
+	const limit = Number(req.query.limit) || 10;
+	const skip = (page - 1) * limit;
+
+	result = result.skip(skip).limit(limit)
+	const products = await result;
+	res.status(200).json({ products, nbHits: products.length });
 };
--- models/product.model.js
+++ models/product.model.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose');
 const productSchema = new mongoose.Schema({
 	name: {
 		type: String,
-		required: [true, 'product price must be provided'],
+		required: [true, 'product name must be provided'],
+	},
+	price: {
+		type: Number,
+		required: [true, 'Product price must be provided.'],
 	},
 	featured: {
 		type: Boolean,
